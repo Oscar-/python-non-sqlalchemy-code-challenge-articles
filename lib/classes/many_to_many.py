5bec6a46755f2274a03ce9a9209aef5e1ce350e5
Oscar Cabrera <cabreraoscar0521@gmail.com>
2024-08-14 12:50:33 -0500

    code challenge completed

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,38 +1,120 @@
 class Article:
+
+    all = [] 
+
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article.all.append(self)
+
+    @property 
+    def title(self):
+        return self._title 
+
+    @title.setter
+    def title(self, value):
+        if (isinstance(value, str) and 5 <= len(value) <= 50 and not hasattr(self, 'title')):
+            self._title = value 
+
+    @property
+    def author(self):
+        return self._author 
+    
+    @author.setter
+    def author(self, value):
+        if isinstance(value, Author):
+            self._author = value 
+
+    @property
+    def magazine(self):
+        return self._magazine
+    
+    @magazine.setter
+    def magazine(self, value):
+        if isinstance(value, Magazine): 
+            self._magazine = value
+
+    def __repr__(self):
+        return f'<Article author={self.author} magazine={self.magazine} title={self.title} /> ' 
         
 class Author:
+    
+    all = []
+
     def __init__(self, name):
         self.name = name
+        Author.all.append(self) 
 
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if(isinstance(value, str) and len(value) > 0 and not hasattr(self, 'name')):
+            self._name = value
+    
+    def __repr__(self): 
+        return f'<Author name={self.name} />'
+           
     def articles(self):
-        pass
+        return [article for article in Article.all if article.author == self]
 
     def magazines(self):
-        pass
+        return list(set([article.magazine for article in self.articles()]))
 
     def add_article(self, magazine, title):
-        pass
+        return Article(self, magazine, title)
 
     def topic_areas(self):
-        pass
+         if not self.articles():
+            return None
+         return list(set([article.magazine.category for article in self.articles()]))
+
 
 class Magazine:
+
+    all = []
+
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        Magazine.all.append(self)
+
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, value):
+        if (isinstance(value, str) and 2 <= len(value) <= 16): 
+            self._name = value
+
+    @property
+    def category(self):
+        return self._category
+    
+    @category.setter
+    def category(self, value):
+        if (isinstance(value, str) and len(value) > 0): 
+            self._category = value 
+
+    def __repr__(self):
+        return f'<Magazine name={self.name} category={self.category} />'
 
     def articles(self):
-        pass
+        return [article for article in Article.all if article.magazine == self]
 
     def contributors(self):
-        pass
+        return list(set([article.author for article in self.articles()]))
 
     def article_titles(self):
-        pass
+        if not self.articles():
+            return None 
+        return [article.title for article in self.articles()]
 
     def contributing_authors(self):
-        pass
+       if len([article.author for article in self.articles()]) <= 2:
+            return None
+       return [article.author for article in self.articles()]
--- lib/debug.py
+++ lib/debug.py
@@ -5,6 +5,29 @@ from classes.many_to_many import Article
 from classes.many_to_many import Author
 from classes.many_to_many import Magazine
 
+# Creating 5 instances of the Author class
+author1 = Author("Jane Austen")
+author2 = Author("Mark Twain")
+author3 = Author("Charles Dickens")
+author4 = Author("George Orwell")
+author5 = Author("Virginia Woolf")
+
+# Creating 5 instances of the Magazine class
+magazine1 = Magazine("National Geo", "Science")
+magazine2 = Magazine("Time", "News")
+magazine3 = Magazine("Vogue", "Fashion")
+magazine4 = Magazine("The New Yorker", "Culture")
+magazine5 = Magazine("Forbes", "Business")
+
+# Creating instances of Article
+article1 = Article(author1, magazine1, "Exploring the Natural World")
+article2 = Article(author2, magazine2, "The Changing Times")
+article3 = Article(author3, magazine3, "The Fashion of the Past")
+article4 = Article(author4, magazine4, "Cultural Shifts")
+article5 = Article(author5, magazine5, "The Business of Writing")
+
+
+
 if __name__ == '__main__':
     print("HELLO! :) let's debug :vibing_potato:")
 
