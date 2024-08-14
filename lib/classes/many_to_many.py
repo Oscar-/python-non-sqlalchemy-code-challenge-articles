74a9fe148d1b4296fd4778e5cb0e4e2b55b41441
Oscar Cabrera <cabreraoscar0521@gmail.com>
2024-08-14 13:17:35 -0500

    added pseudo code

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,5 +1,6 @@
-class Article:
 
+class Article:
+    #class attribute 'all'
     all = [] 
 
     def __init__(self, author, magazine, title):
@@ -8,6 +9,7 @@ class Article:
         self.title = title
         Article.all.append(self)
 
+    #decorators for 'title'
     @property 
     def title(self):
         return self._title 
@@ -17,6 +19,7 @@ class Article:
         if (isinstance(value, str) and 5 <= len(value) <= 50 and not hasattr(self, 'title')):
             self._title = value 
 
+    #decorators for 'author'
     @property
     def author(self):
         return self._author 
@@ -26,6 +29,7 @@ class Article:
         if isinstance(value, Author):
             self._author = value 
 
+    #decorators for 'magazine'
     @property
     def magazine(self):
         return self._magazine
@@ -35,17 +39,20 @@ class Article:
         if isinstance(value, Magazine): 
             self._magazine = value
 
+    #repr for Article
     def __repr__(self):
         return f'<Article author={self.author} magazine={self.magazine} title={self.title} /> ' 
         
 class Author:
     
+    #class attribute 'all'
     all = []
 
     def __init__(self, name):
         self.name = name
         Author.all.append(self) 
 
+    #decorators for 'name'
     @property
     def name(self):
         return self._name
@@ -55,18 +62,27 @@ class Author:
         if(isinstance(value, str) and len(value) > 0 and not hasattr(self, 'name')):
             self._name = value
     
+    #repr for Author
     def __repr__(self): 
         return f'<Author name={self.name} />'
-           
+
+    # method for articles
+    # returning all articles associated with the author        
     def articles(self):
         return [article for article in Article.all if article.author == self]
 
+    # method for magazines
+    # returning a unique list of magazines matching the author 
     def magazines(self):
         return list(set([article.magazine for article in self.articles()]))
 
+    #method for add_article 
+    # returning a new instance of Article with 'name', 'magazine', 'title'
     def add_article(self, magazine, title):
         return Article(self, magazine, title)
 
+    # method for topic_areas
+    # returning a unique list of categories that match the auther, if none, return None 
     def topic_areas(self):
          if not self.articles():
             return None
@@ -75,6 +91,7 @@ class Author:
 
 class Magazine:
 
+    #class attribute 'all'
     all = []
 
     def __init__(self, name, category):
@@ -82,6 +99,7 @@ class Magazine:
         self.category = category
         Magazine.all.append(self)
 
+    #decorators for name 
     @property
     def name(self):
         return self._name
@@ -91,6 +109,7 @@ class Magazine:
         if (isinstance(value, str) and 2 <= len(value) <= 16): 
             self._name = value
 
+    #decorators for category
     @property
     def category(self):
         return self._category
@@ -100,20 +119,29 @@ class Magazine:
         if (isinstance(value, str) and len(value) > 0): 
             self._category = value 
 
+    #repr for Magazine
     def __repr__(self):
         return f'<Magazine name={self.name} category={self.category} />'
 
+    # method for articles
+    # returning all magazines associated with the author 
     def articles(self):
         return [article for article in Article.all if article.magazine == self]
 
+    # method for contributors 
+    # return a unique list of authors that match magazine 
     def contributors(self):
         return list(set([article.author for article in self.articles()]))
 
+    # method for articles_titles
+    # return a list of titles that were written by the magazine, if none, return None
     def article_titles(self):
         if not self.articles():
             return None 
         return [article.title for article in self.articles()]
 
+    # method for contributing_authors
+    # return a list of authors who have more than 2 articles, if less than 2, return None 
     def contributing_authors(self):
        if len([article.author for article in self.articles()]) <= 2:
             return None
